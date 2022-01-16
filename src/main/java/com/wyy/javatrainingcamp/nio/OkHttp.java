8b843045e99cbc8596319539c862a75c5d4b402d
wangyuanyuan32 <wangyuanyuan32@meituan.com>
2022-01-16 22:38:12 +0800

    fix

---
+++ src/main/java/com/wyy/javatrainingcamp/nio/OkHttp.java
@@ -0,0 +1,53 @@
+package com.wyy.javatrainingcamp.nio;
+
+import okhttp3.*;
+import org.springframework.util.CollectionUtils;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Objects;
+
+public class OkHttp {
+
+    public static void main(String[] args) {
+        httpGet("https://www.baidu.com/");
+    }
+
+    /**
+     * 发起get请求
+     *
+     */
+    public static String httpGet(String url) {
+        String result = null;
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(url).build();
+        try {
+            Response response = client.newCall(request).execute();
+            result = response.body().string();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    /**
+     * Post请求
+     */
+    public static Response httpPost(String url, String jsonStr, Map<String, String> headers) throws IOException {
+
+        RequestBody requestBody = RequestBody.create(MediaType.parse("text/html;charset=utf-8"), jsonStr);
+        Request.Builder requestBuilder = new Request.Builder().url(url).post(requestBody);
+
+        if(!CollectionUtils.isEmpty(headers)){
+            for (Map.Entry<String, String> mapEntry : headers.entrySet()) {
+                if(!Objects.isNull(mapEntry.getValue())){
+                    requestBuilder.addHeader(mapEntry.getKey(), String.valueOf(mapEntry.getValue()));
+                }
+            }
+        }
+
+        Response response =  new OkHttpClient().newCall(requestBuilder.build()).execute();
+        return response;
+    }
+
+}
